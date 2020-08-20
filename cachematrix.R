e1ae080d5cb9f764d326c63b3c12026a657dd876
Yun Yu <yun.yu.y@gmail.com>
2020-08-20 09:32:56 -0700

    first draft

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        I <- NULL                  # initiate I as NULL      
+        
+        set <- function(y){     
+                x <<- y            # assign the input to the x object in the parent environment
+                I <<- NULL         # assign NULL to I in the parent environment
+        }
+        
+        get <- function() x        # define getter for matrix x, retrives x from parent enviroment
+        
+        setInverse <- function(inverse) I <<- inverse
+        
+        getInverse <- function()I
+        
+        list(set = set, 
+             get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 
 }
 
@@ -11,5 +28,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        I <- x$getInverse()
+        
+        # Check if I is pre-exists, return cached data
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        
         ## Return a matrix that is the inverse of 'x'
+        data <- x$get()
+        I <- solve(data, ...)   # calculate inversed matrix
+        x$setInverse(I)
+        I
+        
 }
